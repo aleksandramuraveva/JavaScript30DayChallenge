d5400fbdab03b8fc44b171a6464669b6635151c8
Aleksandra Muraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2021-06-11 12:16:56 +0600

    Add my final solution

--- 2.JS and CSS Clock/script.js
+++ 2.JS and CSS Clock/script.js
@@ -1,4 +1,7 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
+
 
 
 	
@@ -34,12 +37,21 @@ const secondHand = document.querySelector(".second-hand");
 
 
 
-setInterval(function showSeconds(){
+setInterval(function showTime(){
 	let time = new Date()
+	let hours = time.getHours();
 	let seconds = time.getSeconds();
-	let degrees = (seconds * 6) + 90
-	console.log(seconds)
-	secondHand.style.transform = `rotate(${degrees}deg)`
+	let minutes = time.getMinutes();
+	let hdegrees = (hours * 30) + 90;
+	let mdegrees = (minutes * 6)+ 90;
+	let sdegrees = (seconds * 6) + 90;
+	console.log(time);
+	console.log(hours);
+	console.log(minutes);
+	console.log(seconds);
+	hourHand.style.transform = `rotate(${hdegrees}deg)`;
+	minuteHand.style.transform = `rotate(${mdegrees}deg)`;
+	secondHand.style.transform = `rotate(${sdegrees}deg)`;
 },1000)
 
 
